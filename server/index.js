99d7c7c8e6eda1f0b0414f98d678d3162ee93a93
Andrew Kwong <kwong.andy93@gmail.com>
2020-11-13 14:23:49 -0800

    added router to handle size requests

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const sizesRouter = require('./routers/sizes.js');
+const mongoose = require('mongoose');
+const port = 3003;
+
+mongoose.connect('mongodb://localhost/sizepicker',
+  { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to sizepicker DB'));
 
 app.use(bodyParser.text());
+//Uncomment when webpack is configured
+//app.use(express.static(__dirname + '../client/dist'));
 
-app.get('/api/sizes/:itemId', (req, res) => {
+app.use('/api/sizes', sizesRouter);
 
+app.listen(port, () => {
+  console.log(`Spice traders on port ${port}`);
 })
