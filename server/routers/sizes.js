213a7fcf5cd5470dc76cb6b2d49d43bca79334a1
Andrew Kwong <kwong.andy93@gmail.com>
2020-11-14 12:15:37 -0800

    install debug module

--- server/routers/sizes.js
+++ server/routers/sizes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
+const debug = require('debug')('app:server:sizes');
 const productDb = require('../../db/models/product.js');
 const router = express.Router();
 
 router.route('/').get(async (req, res) => {
   const product = await productDb.findOne(0).catch((ex) => {
-    console.log(ex);
+    debug(ex);
     res.status(404).send(ex.message);
   });
   res.send(product);
@@ -14,7 +15,7 @@ router.route('/').get(async (req, res) => {
 router.route('/:itemId').get(async (req, res) => {
   const itemId = req.params.itemId;
   const product = await productDb.findOne(itemId).catch((ex) => {
-    console.log(ex);
+    debug(ex);
     res.status(404).send(ex.message);
   });
   res.send(product);
