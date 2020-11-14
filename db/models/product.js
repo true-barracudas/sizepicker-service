66ecf51d0c74a5d102e8469e5f41d5a10b5eeb46
Andrew Kwong <kwong.andy93@gmail.com>
2020-11-13 20:32:38 -0800

    refactors for cleanliness

--- db/models/product.js
+++ db/models/product.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-return-await */
 const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
@@ -7,33 +8,28 @@ const productSchema = mongoose.Schema({
   color: String,
   price: Number,
   photo_url: String,
-  skus: [{ size: String, stock: Number }]
-})
+  skus: [{ size: String, stock: Number }],
+});
 
 const ProductModel = mongoose.model('Product', productSchema);
 
-const findAll = async () => {
-  return await ProductModel.find({});
-}
+const findAll = async () => ProductModel.find({});
 
-const findOne = async (id) => {
-  return await ProductModel.findOne({id});
-}
+const findOne = async (id) => ProductModel.findOne({ id });
 
-const insertOneOrMany = async (productOrProducts) => {
-  return await ProductModel.create(productOrProducts);
-}
+const insertOneOrMany = async (productOrProducts) => ProductModel.create(productOrProducts);
 
+// eslint-disable-next-line no-unused-vars
 const dropCollection = async () => {
   try {
-    return await ProductModel.remove({})
+    return await ProductModel.remove({});
   } catch (ex) {
     console.log(ex.message);
   }
-}
+};
 
 module.exports = {
   findAll,
   findOne,
-  insertOneOrMany
-}
+  insertOneOrMany,
+};
