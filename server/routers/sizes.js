ee65b2384f8953e7c1a2c63e1e715f0a049ce27f
Andrew Kwong <kwong.andy93@gmail.com>
2020-11-14 12:28:48 -0800

    fix eslint and style changes

--- server/routers/sizes.js
+++ server/routers/sizes.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const app = express();
 const debug = require('debug')('app:server:sizes');
 const productDb = require('../../db/models/product.js');
+
 const router = express.Router();
 
 router.route('/').get(async (req, res) => {
@@ -10,15 +10,15 @@ router.route('/').get(async (req, res) => {
     res.status(404).send(ex.message);
   });
   res.send(product);
-})
+});
 
 router.route('/:itemId').get(async (req, res) => {
-  const itemId = req.params.itemId;
+  const { itemId } = req.params;
   const product = await productDb.findOne(itemId).catch((ex) => {
     debug(ex);
     res.status(404).send(ex.message);
   });
   res.send(product);
-})
+});
 
 module.exports = router;
